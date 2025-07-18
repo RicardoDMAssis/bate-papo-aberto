06a73b7f9b083febc6b8f7ecadde80c1a6870ad8
RicardoDMAssis <ricardo.assis@undf.edu.br>
2025-07-18 00:07:05 -0300

    cross origins modificado

--- src/main/java/com/ricardo/sala_aberta/controller/MensagemController.java
+++ src/main/java/com/ricardo/sala_aberta/controller/MensagemController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/mensagens")
 public class MensagemController {
--- src/main/java/com/ricardo/sala_aberta/controller/UsuarioController.java
+++ src/main/java/com/ricardo/sala_aberta/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
